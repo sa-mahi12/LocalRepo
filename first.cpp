49a71b7768d8d86c0b2a86384d35b55d0e783eb7
sanjida-mahi <sanjidamahi2005@gmail.com>
2025-05-19 00:05:33 +0600

    add initial files

---
+++ first.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+int main(){
+    vector<int>vec={1,2,3,4,5};
+    for(int val:vec){
+        cout << val <<endl;
+    }
+    return 0;
+}
